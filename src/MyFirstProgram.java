04d4261fa0a87c14b29dcb0eec6deb08e634fb93
CodeEgale <vaishnavirduratkar.cse22f@kdkce.edu.in>
2023-08-20 15:19:07 +0530

    Java Program

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -1,7 +1,6 @@
 public class MyFirstProgram {
-    public static void main(String[] args){
-      /* Write your code here */
+    public static void main(String[] args) {
+        // write your code here
         System.out.println("Hello World !!!");
     }
 }
-
